ea17877256de021292beaf4f845308b58dca788f
bodhi <bodwang@deloitte.com.cn>
2018-01-31 11:35:24 +0800

    增加删除本地过期备份

--- scripts/xtrabackup.sh
+++ scripts/xtrabackup.sh
@@ -10,3 +10,4 @@ BASE_DIR=$(grep base_dir: ${SCRIPT_HOME}dbavars.yml|awk '{print $2}')
 EXPIRED_DAYS=$(grep expired_days: ${SCRIPT_HOME}dbavars.yml|awk '{print $2}')
 
 cd ${SCRIPT_HOME} && ansible-playbook xtrabackup.yml --extra-vars="backup_date=$PRCSS_DATE" | tee /var/log/xtrabackup/$PRCSS_DATE.log
+find ${BASE_DIR:-/data/backups/full} -mtime +${EXPIRED_DAYS:-15} -type d -print0 | xargs -0 rm -rf
